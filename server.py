f7cc41086db479a049281760f9b5977014e690a0
yilmazmuhammed <yilmazmu15@itu.edu.tr>
2018-10-17 18:20:16 +0300

    Merge server files for heroku server.py, settings.py, and views.py files merged because heroku cannot deploy

--- server.py
+++ server.py
@@ -1,20 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-import views
+app = Flask(__name__)
 
 
-def create_app():
-    app = Flask(__name__)
-    # app.config.from_object("settings")
+@app.route("/")
+def home_page():
+    return render_template("home.html")
 
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/books", view_func=views.books_page)
-
-    return app
+@app.route("/books")
+def books_page():
+    return render_template("books.html")
 
 
 if __name__ == "__main__":
-    app = create_app()
-    # port = app.config.get("PORT", 5000)
-    # app.run(host="0.0.0.0", port=port)
     app.run()
--- views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-
-def home_page():
-    return render_template("home.html")
-
-
-def books_page():
-    return render_template("books.html") 
