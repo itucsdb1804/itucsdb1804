1404365e974093e886a2180c7111400285bedef8
yilmazmuhammed <yilmazmu15@itu.edu.tr>
2018-10-17 17:21:45 +0300

    Split server file Create settings.py for server settings Create views.py for pages Update server.py

---
+++ views.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from flask import render_template
+
+
+def home_page():
+    return render_template("home.html")
+
+
+def books_page():
+    return render_template("books.html") 
