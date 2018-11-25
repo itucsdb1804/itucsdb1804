e8ad06821503d74df1c3bff92d17aae147b52236
Ahmed Yasin Kul <kul15@itu.edu.tr>
2018-11-25 22:22:26 +0300

    Added Customer functions to database.py and class to tables.py files.

--- database.py
+++ database.py
@@ -11,6 +11,13 @@ class Database:
             print("Usage: DATABASE_URL=url python database.py", file=sys.stderr)
             sys.exit(1)
         self.book = self.Book(url)
+        self.customer = self.Customer(url)
+        #self.person = self.Person(url)
+        #self.address = self.Address(url)
+        #self.author = self.Author(url)
+        #self.book_author = self.Book_Author(url)
+        #self.category = self.Category(url)
+        #self.book_category = self.Book_Category(url)
 
     # myilmaz
     class Book:
@@ -74,3 +81,72 @@ class Database:
                 cursor.close()
 
             return books
+
+
+    class Customer:
+        def __init__(self, url):
+            self.url = url
+            self.dbname = "CUSTOMER"
+
+
+        def add(self, customer):
+            query = "INSERT INTO CUSTOMER (PERSON_ID, USERNAME, EMAIL, PASS_HASH, PHONE, IS_ACTIVE) VALUES (%s, %s, %s, %s, %s, %s)"    
+            fill = (customer.person_id, customer.username, customer.email, customer.pass_hash, customer.phone, customer.is_active)
+
+            with dbapi2.connect(self.url) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query, fill)
+                cursor.close()
+
+
+        def update(self, customer):
+            query = "UPDATE CUSTOMER SET USERNAME = %s, EMAIL = %s, PASS_HASH = %s, PHONE = %s, IS_ACTIVE = %s WHERE (CUSTOMER_ID = %s)"
+            fill = (customer.username, customer.email, customer.pass_hash, customer.phone, customer.is_active, customer.customer_id)
+
+            with dbapi2.connect(self.url) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query, fill)
+                cursor.close()
+
+
+        def delete(self, customer):
+            query = "DELETE FROM CUSTOMER WHERE CUSTOMER_ID = %s"
+            fill = (customer.customer_id)
+
+            with dbapi2.connect(self.url) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query, fill)
+                cursor.close()
+
+
+        def get_row(self, cust_id):
+            _customer = None
+
+            query = "SELECT * FROM CUSTOMER WHERE CUSTOMER_ID = %s"
+            fill = (cust_id)
+
+            with dbapi2.connect(self.url) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query, fill)
+                customer = cursor.fetchone()
+                if customer is not None:
+                    _customer = Customer(customer[0], customer[1], customer[2], customer[3], customer[4], customer[5], customer[6])
+
+            return _customer
+
+        def get_table(self):
+            customers = []
+
+            query = "SELECT * FROM CUSTOMER;"
+
+            with dbapi2.connect(self.url) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query)
+                for customer in cursor:
+                    customer_ = Customer(customer[0], customer[1], customer[2], customer[3], customer[4], customer[5], customer[6])
+                    customers.append(customer_)
+                cursor.close()
+
+            return customers
+
+
--- tables.py
+++ tables.py
@@ -4,3 +4,14 @@ class Book:
         self.book_name = book_name
         self.release_year = release_year
         self.explanation = explanation
+
+
+class Customer:
+    def __init__(self, customer_id, person_id, username, email, password_hash, phone, is_active):
+        self.customer_id = customer_id
+        self.person_id = person_id
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+        self.phone = phone
+        self.is_active = is_active
